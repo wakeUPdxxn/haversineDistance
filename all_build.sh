9606daf278db55b6d601a57bd67921eb4a1ab78e
DXXN <pexwp@mail.ru>
2024-10-07 03:20:53 +0300

    small synt. fix

--- all_build.sh
+++ all_build.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 if [[ "$OSTYPE" == "msys" ]]; 
 then
    cmake -S . -B build/release -G "MinGW Makefiles" -D CMAKE_BUILD_TYPE=Release
-   cmake --build build/release'
+   cmake --build build/release
 else
    cmake -S . -B build/release -D CMAKE_BUILD_TYPE=Release
-   cmake --build build/release'
+   cmake --build build/release
 fi
